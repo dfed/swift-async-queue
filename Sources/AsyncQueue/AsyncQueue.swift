16e00d3a7e3e2ceadcedb2c1b8f52b6e6238afd4
Dan Federman <dfed@me.com>
2022-11-22 21:17:38 -0800

    Create AsyncQueue

--- Sources/AsyncQueue/AsyncQueue.swift
+++ Sources/AsyncQueue/AsyncQueue.swift
@@ -20,4 +20,70 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-final class AsyncQueue {}
+/// A queue that enables ordered sending of events from synchronous to asynchronous code
+public final class AsyncQueue: Sendable {
+
+    // MARK: Initialization
+
+    /// Instantiates an asynchronous queue.
+    /// - Parameter priority: The priority of the tasks added to the asynchronous queue.
+    public init(priority: TaskPriority = .medium) {
+        var capturedTaskStreamContinuation: AsyncStream<@Sendable () async -> Void>.Continuation? = nil
+        let taskStream = AsyncStream<@Sendable () async -> Void> { continuation in
+            capturedTaskStreamContinuation = continuation
+        }
+        taskStreamContinuation = capturedTaskStreamContinuation
+
+        streamTask = Task.detached(priority: priority) {
+            for await task in taskStream {
+                await task()
+            }
+        }
+    }
+
+    deinit {
+        taskStreamContinuation.finish()
+    }
+
+    // MARK: Public
+
+    /// Schedules an asynchronous task for execution and immediately returns.
+    /// The schedueled task will not execute until all prior tasks have completed.
+    /// - Parameter task: The task to enqueue.
+    public func async(_ task: @escaping @Sendable () async -> Void) {
+        taskStreamContinuation.yield(task)
+    }
+
+    /// Schedules an asynchronous throwing task and returns after the task is complete.
+    /// The schedueled task will not execute until all prior tasks have completed.
+    /// - Parameter task: The task to enqueue.
+    /// - Returns: The value returned from the enqueued task.
+    public func await<T>(_ task: @escaping @Sendable () async -> T) async -> T {
+        await withUnsafeContinuation { continuation in
+            taskStreamContinuation.yield {
+                continuation.resume(returning: await task())
+            }
+        }
+    }
+
+    /// Schedules an asynchronous task and returns after the task is complete.
+    /// The schedueled task will not execute until all prior tasks have completed.
+    /// - Parameter task: The task to enqueue.
+    /// - Returns: The value returned from the enqueued task.
+    public func await<T>(_ task: @escaping @Sendable () async throws -> T) async throws -> T {
+        try await withUnsafeThrowingContinuation { continuation in
+            taskStreamContinuation.yield {
+                do {
+                    continuation.resume(returning: try await task())
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+
+    // MARK: Private
+
+    private let streamTask: Task<Void, Never>
+    private let taskStreamContinuation: AsyncStream<@Sendable () async -> Void>.Continuation!
+}
--- Tests/AsyncQueueTests/AsyncQueueTests.swift
+++ Tests/AsyncQueueTests/AsyncQueueTests.swift
@@ -26,8 +26,149 @@ import XCTest
 
 final class AsyncQueueTests: XCTestCase {
 
-    func test_example() {
-        _ = AsyncQueue()
+    // MARK: XCTestCase
+
+    override func setUp() async throws {
+        try await super.setUp()
+
+        systemUnderTest = AsyncQueue()
+    }
+
+    override func tearDown() async throws {
+        try await super.tearDown()
+
+        await systemUnderTest.await { /* Drain the queue */ }
+    }
+
+    // MARK: Behavior Tests
+
+    func test_async_sendsEventsInOrder() {
+        let counter = Counter()
+        for iteration in 1...1_000 {
+            systemUnderTest.async {
+                await counter.incrementAndExpectCount(equals: iteration)
+            }
+        }
+    }
+
+    func test_async_executesAsyncBlocksSeriallyAndAtomically() {
+        let unsafeCounter = UnsafeCounter()
+        for iteration in 1...1_000 {
+            systemUnderTest.async {
+                unsafeCounter.incrementAndExpectCount(equals: iteration)
+            }
+        }
+    }
+
+    func test_async_isNotReentrant() async {
+        let counter = Counter()
+        await systemUnderTest.await { [systemUnderTest] in
+            systemUnderTest.async {
+                await counter.incrementAndExpectCount(equals: 2)
+            }
+            await counter.incrementAndExpectCount(equals: 1)
+            systemUnderTest.async {
+                await counter.incrementAndExpectCount(equals: 3)
+            }
+        }
+    }
+
+    func test_async_retainsReceiverUntilFlushed() async {
+        var systemUnderTest: AsyncQueue? = AsyncQueue()
+        let counter = Counter()
+        let expectation = self.expectation(description: #function)
+        await withThrowingTaskGroup(of: Void.self) { taskGroup in
+            let foreverSleep = Task {
+                try await Task.sleep(nanoseconds: UInt64.max)
+            }
+            taskGroup.addTask {
+                try await foreverSleep.value
+            }
+            systemUnderTest?.async {
+                // Make the queue wait.
+                try? await foreverSleep.value
+                await counter.incrementAndExpectCount(equals: 1)
+            }
+            systemUnderTest?.async {
+                // This async task should not execute until the sleep is cancelled.
+                await counter.incrementAndExpectCount(equals: 2)
+                expectation.fulfill()
+            }
+            // Nil out our reference to the queue to show that the enqueued tasks will still complete
+            systemUnderTest = nil
+            // Cancel the sleep timer to unlock the remaining enqueued tasks.
+            foreverSleep.cancel()
+
+            await waitForExpectations(timeout: 1.0)
+        }
+    }
+
+    func test_await_sendsEventsInOrder() async {
+        let counter = Counter()
+        for iteration in 1...1_000 {
+            systemUnderTest.async {
+                await counter.incrementAndExpectCount(equals: iteration)
+            }
+
+            guard iteration % 25 == 0 else {
+                // Keep sending async events to the queue.
+                continue
+            }
+
+            await systemUnderTest.await {
+                let count = await counter.count
+                XCTAssertEqual(count, iteration)
+            }
+        }
+    }
+
+    func test_await_canReturn() async {
+        let expectedValue = UUID()
+        let returnedValue = await systemUnderTest.await { expectedValue }
+        XCTAssertEqual(expectedValue, returnedValue)
+    }
+
+    func test_await_canThrow() async {
+        struct TestError: Error, Equatable {
+            private let identifier = UUID()
+        }
+        let expectedError = TestError()
+        do {
+            try await systemUnderTest.await { throw expectedError }
+        } catch {
+            XCTAssertEqual(error as? TestError, expectedError)
+        }
+    }
+
+    // MARK: Private
+
+    private var systemUnderTest = AsyncQueue()
+
+    // MARK: - Counter
+
+    private actor Counter {
+        func incrementAndExpectCount(equals expectedCount: Int) {
+            increment()
+            XCTAssertEqual(expectedCount, count)
+        }
+
+        func increment() {
+            count += 1
+        }
+
+        var count = 0
+    }
+
+    // MARK: - UnsafeCounter
+
+    /// A counter that is explicitly not safe to utilize concurrently.
+    private final class UnsafeCounter: @unchecked Sendable {
+        func incrementAndExpectCount(equals expectedCount: Int) {
+            count += 1
+            XCTAssertEqual(expectedCount, count)
+        }
+
+        var count = 0
     }
 
 }
